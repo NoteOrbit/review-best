75a418d5ac895703d1dde1af4f93583b2f15c03a
NoteOrbit <notetyofsequence@gmail.com>
2025-08-27 17:36:18 +0700

    Update GitHub Actions workflow and Terraform configuration for Databricks
    - Upgrade actions/checkout to v4 and hashicorp/setup-terraform to v3 for improved functionality.
    - Simplify Terraform notebook resource configuration by removing unnecessary comments.
    - Enhance Terraform Apply step to use the generated plan for better auditing.

--- infra/terraform/databricks.tf
+++ infra/terraform/databricks.tf
@@ -10,10 +10,7 @@ locals {
 resource "databricks_notebook" "pipeline_notebooks" {
   for_each = local.notebooks
 
-  # The 'source' attribute is simpler than 'content_base64'
-  # This path assumes your 'notebooks' folder is one level above your terraform files.
   source = "${path.module}/../notebooks/${each.value}"
   
-  # The path where the notebook will be created in the Databricks workspace
   path     = "/Shared/notebooks/${each.value}"
 }
